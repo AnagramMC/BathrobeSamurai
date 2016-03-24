ad6a849cd74034b001bafcafa0d0202e68949c51
Zachary Truckel <zachary.truckel@zacharytruckelgames.ca>
2016-03-23 23:00:58 -0400

    Tera Bug started
    made all the scripts for the tera bug (Character nd Controller)

---
+++ Source/BathrobeSamurai/AI/TeraAI/TeraAIController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AI/BaseAIController.h"
+#include "TeraAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATHROBESAMURAI_API ATeraAIController : public ABaseAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
